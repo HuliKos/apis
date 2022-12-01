a7663a0e70ec77f04fa8012a184d4789c2172757
Lan Kostrikin <lankostrikin@gmail.com>
2022-12-01 09:14:57 -0600

    Final Version

--- swapi/main.js
+++ swapi/main.js
@@ -1,19 +1,25 @@
 // const { default: axios } = require("axios");
 
-function buttonClick (evt) {
-    evt.preventDefault();
-    console.log(`button clicked`)
+// const { default: axios } = require("axios")
+
+// const { default: axios } = require("axios");
+
+let button = document.querySelector('button')
+
+function buttonHandler () {
+    axios.get(`https://swapi.dev/api/planets/?search=alderaan`).then(res => {
+        console.log(res.data.results[0].residents)
+        let {residents} = res.data.results[0]
+        for (let i=0; i<residents.length; i++){
+            axios.get(residents[i]).then(res =>{
+                console.log(res.data.name)
+                let h2 = document.createElement('h2')
+                h2.textContent = res.data.name
+                let body = document.querySelector('body')
+                body.appendChild(h2)
+            })
+        }
+    })
 }
 
-let buttonClicked = document.querySelector('button')
-buttonClicked.addEventListener('click',buttonClick)
-
-const callback () =>{
-    let {residents} = planets
-
-    for(i=0; i<residents.length; i++)
-    
-    console.log(residents[i])
-} 
-
-const getAlderaan = () => axios.get('https://swapi.dev/api/planets/?search=alderaan').then(callback)
+button.addEventListener('click',buttonHandler)
